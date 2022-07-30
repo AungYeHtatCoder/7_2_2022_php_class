ab58f6cc444e4af3218cb2ba7b9278ce659fd546
AungYeHtatCoder <aungyehatacoder2021@gmail.com>
2022-07-30 14:35:34 +0630

    pdo_course_new_updated_code

--- chapter_04/pdo_course/section_5/conn.php
+++ chapter_04/pdo_course/section_5/conn.php
@@ -1,17 +1,31 @@
 <?php
 
+  //host, dbname, user, password 
+
+  
+
   try {
 
-  	$host = "localhost";
-  	$dbname = "todos";
-  	$user = "root";
-  	$pass = "";
+  	$host     = "localhost";
+	  $dbname   = "blogpost";
+	  $user     = "root";
+	  $password = "";
 
-  	$conn = new PDO ("mysql:host=$host;dbname=$dbname",$user,$pass);
-  	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  $conn = new PDO ("mysql:host=$host;dbname=$dbname", $user, $password);
+	  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   } catch(PDOException $e) {
-
-  	echo "error is: " . $e->getMessage();
-
+     echo $e->getMessage();
+  	//die("db error");
   }
+
+
+  
+ 
+
+
+
+
+
+
+ // echo "the rest of the page";
--- chapter_04/pdo_course/section_5/index.php
+++ chapter_04/pdo_course/section_5/index.php
@@ -1,7 +1,6 @@
-
 <?php
 
-  require "conn.php";
+  require ("conn.php");
   
 
   $data = $conn->query("SELECT * FROM tasks");
@@ -12,52 +11,58 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>todos</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="" crossorigin="">
-		<link rel="stylesheet" href="style.css">
 
-	</head>
-	<body>
-        <div class="container">
-		<form method="POST" action="insert.php" class="form-inline">
-		 
-		  <div class="form-group mx-sm-3 mb-2">
-		    <label for="inputPassword2" class="sr-only">create</label>
-		    <input name="mytask" type="text" class="form-control" id="inputPassword2" placeholder="enter task">
-		  </div>
-		       <button name="submit" type="submit" class="btn btn-primary mb-2">Create</button>
+<head>
+ <title>todos</title>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity=""
+  crossorigin="">
+ <link rel="stylesheet" href="style.css">
 
-		</form>
+</head>
 
-		<table class="table">
-		  <thead>
-		    <tr>
-		      <th>#</th>
-		      <th>Task Name</th>
-		      <th>Action</th>
-		    </tr>
-		  </thead>
-		  <tbody>
-		  	 <?php while($rows = $data->fetch(PDO::FETCH_OBJ)): ?>   
-		    <tr>
-		   
-		     <td><?php echo $rows->id; ?></td>
-		     <td><?php echo $rows->name; ?></td>
-		     <td><a href="delete.php?del_id=<?php echo $rows->id; ?>" class="btn btn-danger">delete</a></td>
+<body>
+ <div class="container">
+  <form method="POST" action="insert.php" class="form-inline">
 
-		    </tr>
-		<?php endwhile; ?>
+   <div class="form-group mx-sm-3 mb-2">
+    <label for="inputPassword2" class="sr-only">create</label>
+    <input name="mytask" type="text" class="form-control" id="inputPassword2" placeholder="enter task">
+   </div>
+   <button name="submit" type="submit" class="btn btn-primary mb-2">Create</button>
 
+  </form>
 
-		  </tbody>
-		</table>
-	</div>
+  <table class="table">
+   <thead>
+    <tr>
+     <th>#</th>
+     <th>Task Name</th>
+     <th>Action</th>
+    </tr>
+   </thead>
+   <tbody>
+    <?php while($rows = $data->fetch(PDO::FETCH_OBJ)): ?>
+    <tr>
+
+     <td><?php echo $rows->id; ?></td>
+     <td><?php echo $rows->name; ?></td>
+     <td><a href="delete.php?del_id=<?php echo $rows->id; ?>" class="btn btn-danger">delete</a></td>
+
+    </tr>
+    <?php endwhile; ?>
 
 
+   </tbody>
+  </table>
+ </div>
 
-		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
 
-	</body>
+
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity=""
+  crossorigin="anonymous"></script>
+
+</body>
+
 </html>
