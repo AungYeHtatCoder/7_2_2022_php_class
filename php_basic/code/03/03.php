32b61fb3c3edf4bfd84cff6cdd152341a796405a
AungYeHtatCoder <aungyehatacoder2021@gmail.com>
2022-08-08 08:27:20 +0630

    php_basic_code_add

---
+++ php_basic/code/03/03.php
@@ -0,0 +1,15 @@
+<?php
+  $mycounter = 1;
+  $mystring  = "Hello";
+  $myarray   = array("One", "Two", "Three");
+
+  echo "The value of \$mycounter is " . $mycounter . "<br>";
+  echo "The value of \$mystring is " . $mystring . "<br>";
+  echo "The value of \$myarray is " . $myarray[2] . "<br>";
+  echo $myarray[0];
+  echo "<br>";
+  echo $myarray[1];
+  echo "<br>";
+
+  echo $myarray[2];
+?>
